605d6114e41e9143398eb1cc02d6d12024a9acf8
GeorgeSemenov <zhora.sem@yandex.ru>
2021-08-30 15:29:39 +0300

    add blog page

--- wp-content/themes/afrianska/functions.php
+++ wp-content/themes/afrianska/functions.php
@@ -1,6 +1,7 @@
 <?php
 function some_assets() {
     wp_enqueue_style( 'maincss', get_template_directory_uri() . '/assets/index.css' );
+    wp_enqueue_style( 'blogcss', get_template_directory_uri() . '/assets/blogpage.css' );
 
     wp_enqueue_script( 'common-script', get_template_directory_uri() . '/assets/js/common.js', array(), '20151215', true );
     wp_enqueue_script( 'main-script', get_template_directory_uri() . '/assets/js/index.js', array(), '20151215', true );
